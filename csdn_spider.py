34b161bffcac95c4aae72928211a107fc5c1391c
ahmay <meizi111082@hotmail.com>
2019-12-04 10:59:30 +0800

    使用chardet第三方库来识别要爬的页面的编码

--- csdn_spider.py
+++ csdn_spider.py
@@ -2,9 +2,12 @@
 #使用 urllib库开发一个最简单的爬虫 爬csdn首页的html代码
 
 from urllib import request
+import  chardet
 
 if __name__ == '__main__':
     response = request.urlopen("https://www.csdn.net/")
     html = response.read()
-    html = html.decode('utf-8')
+    chardet = chardet.detect(html)
+    print(chardet['encoding'])
+    html = html.decode(chardet['encoding'])
     print(html)
